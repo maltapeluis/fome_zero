7d9c30390fd243256ad728737a01a17ebbed9b91
Luis Gustavo <89364750+maltapeluis@users.noreply.github.com>
2024-01-09 22:45:39 -0300

    Update 1_📊_Home.py

--- 1_📊_Home.py
+++ 1_📊_Home.py
@@ -1,7 +1,6 @@
 # ===========================================
 # Importando as bibliotecas necessárias
 # ===========================================
-!pip install inflection
 # importando as bibliotecas necessárias
 import pandas as pd
 import inflection
